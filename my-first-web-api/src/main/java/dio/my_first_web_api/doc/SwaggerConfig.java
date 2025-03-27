bbcf1f708bc40fe3c39057652de1bcf8fd84f8de
Paola Moura <paolamoura.kj@hotmail.com>
2025-03-26 21:37:38 -0300

    doc: configurada UI inicial no Swagger

---
+++ my-first-web-api/src/main/java/dio/my_first_web_api/doc/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package dio.my_first_web_api.doc;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Minha API")
+                        .version("1.0")
+                        .description("Documentação da minha API usando OpenAPI 3"));
+    }
+}
