a2b97eae6364c6ac980675d8aae42553d4c41082
Paola Moura <paolamoura.kj@hotmail.com>
2025-03-26 22:28:48 -0300

    fix: adicionado tratamento de exceção caso o usuário não insira o nome

--- my-first-web-api/src/main/java/dio/my_first_web_api/repository/UserRepository.java
+++ my-first-web-api/src/main/java/dio/my_first_web_api/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package dio.my_first_web_api.repository;
 
+import dio.my_first_web_api.handler.BusinessException;
 import dio.my_first_web_api.model.User;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,9 @@ import java.util.List;
 @Repository
 public class UserRepository {
     public void save(User usuario) {
+        if(usuario.getLogin()==null){
+            throw new BusinessException("O campo login é obrigatório");
+        }
         if(usuario.getId() == null) {
             System.out.println("SAVE - Recebendo o usuário na camada de repositório");
         }else {
